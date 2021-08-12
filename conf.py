072843237117af0759287e4220e63eff5879b00b
mirik070 <75161328+mirik070@users.noreply.github.com>
2021-08-12 20:29:52 +0500

    remove conf

--- conf.py
+++ conf.py
@@ -1,8 +1,1 @@
-SERVER_URI = 'http://localhost:8000'
-BOT_TOKEN = '1099481391:AAGJda-1y0s5dOV8NUtZ2Rk9OxckGboAvvk'
-ORDER_URI = 'http://shop.khm473.ru/'
-ADMIN_ID = [
-    '38489948',
-    '127127849',
-    '2928488'
-]
+
